33cfa6c38f70c577aa4c9e8be3828f3343b2e518
Charles Ferreira <charles.dev@live.com>
2019-05-07 13:23:32 -0300

    feat(#1): create a new Spring Boot app

---
+++ src/main/java/dev/charlesferreira/upcomingmoviesapi/UpcomingMoviesApiApplication.java
@@ -0,0 +1,13 @@
+package dev.charlesferreira.upcomingmoviesapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class UpcomingMoviesApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(UpcomingMoviesApiApplication.class, args);
+	}
+
+}
