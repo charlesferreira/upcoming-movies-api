fdeb6495117037158ef1f27178c071fbb45e4c16
Charles Ferreira <charles.dev@live.com>
2019-05-07 23:43:55 -0300

    feat(#6): turn GenresController into a service

--- src/main/java/dev/charlesferreira/upcomingmoviesapi/service/GenresService.java
+++ src/main/java/dev/charlesferreira/upcomingmoviesapi/service/GenresService.java
@@ -1,28 +1,23 @@
-package dev.charlesferreira.upcomingmoviesapi.controller;
+package dev.charlesferreira.upcomingmoviesapi.service;
 
 import dev.charlesferreira.upcomingmoviesapi.model.Genre;
 import dev.charlesferreira.upcomingmoviesapi.service.response.GenreListResponse;
-import dev.charlesferreira.upcomingmoviesapi.service.TMDBService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.cache.annotation.Cacheable;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.stereotype.Service;
 
 import java.util.List;
 
-@RestController
-@RequestMapping("/genres")
-public class GenresController {
+@Service
+public class GenresService {
 
     private static final String GET_LIST = "/genre/movie/list";
 
     @Autowired
     TMDBService tmdbService;
 
-    @GetMapping
     @Cacheable("genres")
-    public List<Genre> getList() {
+    public List<Genre> genres() {
         return tmdbService.get(GenreListResponse.class, GET_LIST).getGenres();
     }
 
